3126d154b4eae1aa6451bc33ef476aa26339d027
Marat Kopytjuk <kopytjuk@gmail.com>
2025-02-23 16:11:09 +0100

    Add logging functionality

---
+++ src/utils/logging.py
@@ -0,0 +1,27 @@
+import logging
+
+
+def get_client_logger(name: str | None = None) -> logging.Logger:
+    # create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
+
+
+def get_library_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.addHandler(logging.NullHandler())
+    return logger
