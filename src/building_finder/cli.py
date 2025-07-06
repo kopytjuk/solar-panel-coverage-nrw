ce896b6d637f4a322cf2c45c19395ea926a6205a
Marat Kopytjuk <kopytjuk@gmail.com>
2025-07-06 10:30:27 +0200

    Remove azure dependencies for now

--- src/building_finder/cli.py
+++ src/building_finder/cli.py
@@ -1,9 +1,13 @@
 import logging
 
 import click
+from shapely import box
 
-from building_finder.extract_buildings import extract_buildings, write_gdf_to
+from building_finder.extract_buildings import (
+    extract_buildings_from_gps_polygon,
+)
 from utils.logging import get_client_logger
+from utils.transform import transform_utm32N_to_wgs84
 
 logger = get_client_logger()
 
@@ -12,8 +16,10 @@ logging.getLogger("azure").setLevel(logging.WARNING)
 
 
 @click.command()
-@click.argument("tile_name", type=click.STRING)
-@click.argument("output_location", type=click.Path(exists=False))
+@click.argument("utm_km_x", type=click.INT)
+@click.argument("utm_km_y", type=click.INT)
+@click.argument("output_path", type=click.Path(exists=False))
+@click.option("--extent-km", type=click.INT, default=1)
 @click.option(
     "--with-address-only",
     default=True,
@@ -21,25 +27,33 @@ logging.getLogger("azure").setLevel(logging.WARNING)
     help="Filter buildings to include only those with addresses.",
 )
 def building_finder_cli(
-    tile_name: str,
-    output_location: str,
+    utm_km_x: int,
+    utm_km_y: int,
+    extent_km: int,
+    output_path: str,
     with_address_only: bool,
 ):
     """Extract buildings with their addresses and outline geometries from OpenStreetMap.
-    The area of extraction is defined by the TILE_NAME which follows the nomenclature of OpenGeodata.NRW."""
+    The area of extraction is defined by the lower left tile corner as UTM_KM_X, UTM_KM_Y (and EXTENT_KM)"""
 
-    click.echo(f"Starting building extraction for tile: {tile_name}")
-    click.echo(f"Output will be saved to: {output_location}")
+    utm_m_x = utm_km_x * 1000
+    utm_m_y = utm_km_y * 1000
+    extent_m = extent_km * 1000
 
-    buildings_gdf = extract_buildings(
-        tile_name,
-        with_address=with_address_only,
-    )
+    bbox_extent = (utm_m_x, utm_m_y, utm_m_x + extent_m, utm_m_y + extent_m)
+    bbox_extent_utm = box(*bbox_extent)
 
-    write_gdf_to(
-        output_location,
-        buildings_gdf,
-        f"{tile_name}_buildings.gpkg",
+    # Transform bounding box to WGS84
+    bbox_extent_wgs84 = transform_utm32N_to_wgs84(bbox_extent_utm)
+
+    buildings_gdf = extract_buildings_from_gps_polygon(bbox_extent_wgs84, with_address_only)
+    logger.info(f"Found {len(buildings_gdf)} buildings in the tile!")
+
+    buildings_gdf.to_file(
+        str(output_path),
+        layer="buildings",
+        driver="GPKG",
+        index=True,
     )
 
     logger.info("Building finder complete!")
--- src/building_finder/extract_buildings.py
+++ src/building_finder/extract_buildings.py
@@ -1,9 +1,4 @@
-import io
-import os
-from pathlib import Path
-
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient
+import shapely
 from shapely.geometry import box
 
 from utils import (
@@ -12,7 +7,6 @@ from utils import (
     transform_utm32N_to_wgs84,
     transform_wgs84_to_utm32N,
 )
-from utils.azure import parse_blob_storage_uri
 from utils.logging import get_library_logger
 
 logger = get_library_logger(__name__)
@@ -26,12 +20,17 @@ def extract_buildings(tile_name: str, with_address: bool = False):
     # Transform bounding box to WGS84
     bbox_extent_wgs84 = transform_utm32N_to_wgs84(bbox_extent_utm)
 
-    # Retrieve buildings from OSM within the bounding box
-    buildings_from_bbox = get_buildings_from_bbox(
-        bbox_extent_wgs84.bounds, with_address=with_address
-    )
+    return extract_buildings_from_gps_polygon(bbox_extent_wgs84, with_address)
 
-    logger.info(f"Found {len(buildings_from_bbox)} buildings in the bounding box!")
+
+def extract_buildings_from_gps_polygon(gps_polygon: shapely.Polygon, with_address: bool = False):
+    # Retrieve buildings from OSM within the bounding box
+    buildings_from_bbox = get_buildings_from_bbox(gps_polygon.bounds, with_address=with_address)
+
+    logger.debug(f"Found {len(buildings_from_bbox)} buildings in the polygon!")
+
+    # filter for buildings in the polygon
+    buildings_from_bbox = buildings_from_bbox[buildings_from_bbox.within(gps_polygon)]
 
     # Compute the area for all geometries and store it in a column (m² in UTM32N)
     area_arr = [transform_wgs84_to_utm32N(geom).area for geom in buildings_from_bbox.geometry]
@@ -41,37 +40,3 @@ def extract_buildings(tile_name: str, with_address: bool = False):
         return
 
     return buildings_from_bbox
-
-
-def write_gdf_to(output_location, buildings_from_bbox, file_name):
-    is_remote = "://" in output_location
-    logger.info(f"The output remote storage: {is_remote}")
-    if is_remote:
-        storage_acc, container_name, path = parse_blob_storage_uri(output_location)
-        # assuming user did a `az login`
-        default_credential = DefaultAzureCredential()
-
-        blob_storage_url = f"https://{storage_acc}.blob.core.windows.net"
-        service_client = BlobServiceClient(blob_storage_url, credential=default_credential)
-
-        # Upload the file
-        blob_client = service_client.get_blob_client(
-            container=container_name, blob=os.path.join(path, file_name)
-        )
-        bytestream = io.BytesIO()
-        buildings_from_bbox.to_file(bytestream, layer="buildings", driver="GPKG", index=True)
-        bytestream.seek(0)
-
-        blob_client.upload_blob(bytestream, overwrite=True)
-        logger.info("Writing to Azure Blob Storage completed successfully.")
-    else:
-        output_location = Path(output_location)
-        output_location.mkdir(parents=True, exist_ok=True)
-
-        output_path = output_location / file_name
-        buildings_from_bbox.to_file(
-            str(output_path),
-            layer="buildings",
-            driver="GPKG",
-            index=True,
-        )
